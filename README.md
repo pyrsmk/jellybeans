683d0808a841b43c1259f087bf9a4723f9608369
pyrsmk <dev@dreamysource.fr>
2016-09-08 11:47:42 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+jellybeans 0.1.0
+================
+
+Jellybeans is a simple tool that aims to emphasize all nodes by setting them a random background color. It's really useful when you're setting your vertical rhythm accross your page.
+
+Install
+-------
+
+
+```
+npm install pyrsmk/jellybeans
+```
+
+Use
+---
+
+Include it on your page and it will run by itself.
+
+Based on
+--------
+
+- http://stackoverflow.com/a/26172084/4112863
+- https://www.paulirish.com/2009/random-hex-color-code-snippets/
+
+License
+-------
+
+Published under the [MIT license](http://dreamysource.mit-license.org).
---
+++ jellybeans.js
@@ -0,0 +1,19 @@
+;(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.Jellybeans = factory();
+  }
+}(this, function() {
+/*! jellybeans 0.1.0 (https://github.com/pyrsmk/jellybeans) */
+
+var node,
+    iterator = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_ELEMENT);
+
+while(node = iterator.nextNode()) {
+    node.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+return Jellybeans;
+}));
